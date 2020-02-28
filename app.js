e5878252c423e6d0bd995fda2561a383acb79dde
Deteriator <markgriffith009@gmail.com>
2020-02-28 10:07:31 -0500

    reshuffled how pages are used.

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
 const app = express();
 
 const PORT = 8080;
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.set('view engine', 'ejs');
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(expressLayouts);
+app.use(express.static('views'));
+app.get('/', (req, res) => res.render('layout'))
 
 app.listen(PORT, ()=>{
 	 console.log(`App is Live on Port ${PORT}!`)
